d4003a327b5449b01ff13fcb1515abaf6d2e2b3a
Josh Schneier <josh.schneier@gmail.com>
2012-10-15 18:11:29 -0400

    A protocol instance is a peer
    Started setting up the protocol instances, message id mapping and the beginning
    of implementation of the actual protocol by decoding and encoding bytes.

--- protocols.py
+++ protocols.py
@@ -1,15 +1,40 @@
+import struct
 from twisted.internet.protocol import Protocol, ClientFactory
+
+ID_TO_MSG = {None: 'keep-alive',
+             0: 'choke',
+             1: 'unchoke',
+             2: 'interested',
+             3: 'uninterested',
+             4: 'have',
+             5: 'bitfield',
+             6: 'request',
+             7: 'piece',
+             8: 'cancel',
+             9: 'port'
+             }
 
 class PeerProtocol(Protocol):
 
-    def __init__(self):
-        pass
+    def __init__(self, client, torrent):
+        self.client = client
+        self.torrent = torrent
+        self.am_choking = 1
+        self.am_interested = 0
+        self.peer_choking = 1
+        self.peer_interested = 0
     
     def connectionMade(self):
         self.handshake()
 
+    def decode_len(self, message):
+        return struct.unpack_from('!I', message)
+
+    def is_keep_alive(self, message):
+        return not bool(self.decode_len(message))
+
     def dataReceived(self, data):
-        self.factory.deocde(data)
+        self.deocde_len(data)
 
     def handshake():
         pass

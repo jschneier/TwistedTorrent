2d34839bb5c1526ab4f08ec60ff8e1a372749199
Josh Schneier <josh.schneier@gmail.com>
2012-11-23 00:24:01 -0500

    cleaned up and added some tests

--- tests/test_btencode.py
+++ tests/test_btencode.py
@@ -15,36 +15,31 @@ class TestBTDecode(unittest.TestCase):
     def test_dict(self):
         self.assertEquals({'cow': 'moo', 'spam': 'eggs'}, btdecode('d3:cow3:moo4:spam4:eggse'))
 
-    def test_octal_encode_raises_btdecode_error(self):
+    def test_octal_encode_raises_error(self):
         self.assertRaises(BTDecodeError, btdecode, 'i00e')
 
-    def test_extra_raises_btdecode_error(self):
+    def test_extra_raises_error(self):
         self.assertRaises(BTDecodeError, btdecode, 'l4spamel')
+
+    def test_non_string_raises_error(self):
+        self.assertRaises(BTDecodeError, btdecode, ['foo', 'bar'])
 
 class TestBTEncode(unittest.TestCase):
 
     def test_string(self):
         self.assertEquals('4:trac', btencode('trac'))
 
-class TestEncodeString(unittest.TestCase):
-
-    def test_string(self):
-        self.assertEquals('4:trac', btencode('trac'))
-
-class TestEncodeInt(unittest.TestCase):
-
     def test_int(self):
         self.assertEquals('i3e', btencode(3))
-
-class TestEncodeList(unittest.TestCase):
 
     def test_list(self):
         self.assertEquals('l4:spami3ei4e4:spame', btencode(['spam', 3, 4, 'spam']))
 
-class TestEncodeDict(unittest.TestCase):
-
     def test_dict(self):
         self.assertEquals('d3:cow3:moo4:spam4:eggse', btencode({'cow': 'moo', 'spam': 'eggs'}))
+
+    def test_bad_type_raises_error(self):
+        self.assertRaises(BTEncodeError, btencode, set('foo'))
 
 if __name__ == '__main__':
     unittest.main()

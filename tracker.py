dcf42e24e9aaffabe57683c4b4c13b36a21d9227
Josh Schneier <josh.schneier@gmail.com>
2012-11-28 15:33:12 -0500

    messy but almost complete implementation of udp tracker

--- tracker.py
+++ tracker.py
@@ -29,6 +29,8 @@ class UDPTracker(DatagramProtocol):
         host = yield reactor.resolve(self.host, timeout=(1, 3))
         self.host = host
         reactor.listenUDP(torrent.port, self)
+        peers = yield self.deferred
+        defer.returnValue(peers)
 
     def startProtocol(self):
         """Connect so we can only speak to one peer. Then send out the initial
@@ -44,7 +46,6 @@ class UDPTracker(DatagramProtocol):
 
     def datagramReceived(self, datagram, addr):
         self.timeout.reset(5)
-        print datagram
         if not self.connected:
             assert len(datagram) >= 16
             action, trans_id, conn_id = struct.unpack('!IIQ', datagram)
@@ -62,15 +63,30 @@ class UDPTracker(DatagramProtocol):
             assert action == 1
             assert trans_id == self.trans_id
             datagram = datagram[8:]
-            self.get_peers(datagram)
+            self.peers(datagram)
+            self.timeout.cancel()
             self.transport.loseConnection()
 
+    def peers(self, raw_data):
+        # first 8 bytes is number of seeders and leechers - ignore
+        peers_raw = raw_data[8:]
+
+        #break into 6 byte chunks - 4 for ip 2 for port
+        peers = (peers_raw[i:i+6] for i in range(0, len(peers_raw), 6))
+        hosts_ports = [(socket.inet_ntoa(peer[0:4]),
+                        struct.unpack('!H', peer[4:6])[0])
+                        for peer in peers if len(peer) == 6]
+
+        self.deferred.callback(hosts_ports)
+
+
     def _make_announce_pack(self, action, event):
-        return (struct.pack('!QII', self.connect_id, action, self.trans_id) +
-                    self.torrent.info_hash + self.factory.client.client_id +
-                    struct.pack('!QQQIIIiH', self.torrent.downloaded,
-                        self.torrent.left, self.torrent.uploaded, event, 0,
-                        self.key, -1, self.torrent.port))
+        return struct.pack('!QII20s20sQQQIIIiH', self.connect_id, action,
+                                    self.trans_id, self.torrent.info_hash,
+                                    self.factory.client.client_id,
+                                    self.torrent.downloaded, self.torrent.left,
+                                    self.torrent.uploaded, event, 0, self.key,
+                                    -1, self.torrent.port)
 
     @staticmethod
     def generate_32bit():
@@ -133,7 +149,6 @@ class TrackerClient(object):
     @defer.inlineCallbacks
     def get_peers(self, torrent):
         for url in chain.from_iterable(torrent.announce_list):
-            print url
             try:
                 torrent.tracker_url = url
                 protocol, host, port = self._parse(url)

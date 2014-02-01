f80b91a54b40f77d6a3ccd9a17ce53e466694199
Josh Schneier <josh.schneier@gmail.com>
2014-02-01 18:19:52 -0500

    stupid

--- twistedtorrent/scripts.py
+++ twistedtorrent/scripts.py
@@ -1,6 +1,6 @@
 def torrent():
     import sys
     from twistedtorrent import TorrentClient
-    if len(sys.argv == 1):
+    if len(sys.argv) == 1:
         sys.exit('Usage: torrent <list of torrent files>')
     TorrentClient(sys.argv[1:])
